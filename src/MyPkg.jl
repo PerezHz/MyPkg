40b509d5094d2c93da608fe4409f3506084aee74
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2020-06-22 14:34:19 -0500

    Remove `__init__`, comment `greet`

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -4,12 +4,8 @@ module MyPkg
 
 using TaylorIntegration
 
-greet() = print("Hello World!")
+# greet() = print("Hello World!")
 
 @taylorize xdot2(x, p, t) = (local b2 = 3; b2-x^2)
-
-function __init__()
-    @show methods(TaylorIntegration.jetcoeffs!)
-end
 
 end # module
