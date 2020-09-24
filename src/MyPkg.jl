a913a3dbb7c99ebd358afba1ba9b42c5eeb41cc5
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2020-09-23 23:37:11 -0500

    Working version in julia 1.4 and 1.5

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -4,8 +4,37 @@ module MyPkg
 
 using TaylorIntegration
 
-# greet() = print("Hello World!")
+b1 = 3.0
 
-@taylorize xdot2(x, p, t) = (local b2 = 3; b2-x^2)
+x0 = [1.0, 0.0]
+tT = Taylor1(5)
+x0T = Taylor1.(x0, tT.order)
+dx0T = similar(x0T)
+ω = 1.0
+
+y0T = Taylor1(1.0, tT.order)
+
+ex = :(xdot1(x, p, t) = b1-x^2)
+
+ex2 = :(function harm_osc!(dx, x, p, t)
+local ω = p[1]
+local ω2 = ω^2
+dx[1] = x[2]
+dx[2] = - (ω2 * x[1])
+return nothing
+end)
+
+nex = TaylorIntegration._make_parsed_jetcoeffs(ex)
+nex2 = TaylorIntegration._make_parsed_jetcoeffs(ex2)
+
+@eval $ex
+# @eval $nex
+@eval $ex2
+@eval $nex2
+
+# greet() = TaylorIntegration.jetcoeffs!(Val(xdot1), tT, y0T, nothing)
+# greet(f) = TaylorIntegration._determine_parsing!(true, f, tT, y0T, nothing)
+greet2() = TaylorIntegration.jetcoeffs!(Val(harm_osc!), tT, x0T, dx0T, ω)
+greet2(f) = TaylorIntegration._determine_parsing!(true, f, tT, x0T, dx0T, ω)
 
 end # module
